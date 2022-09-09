96ea7159edb1fa0910d8ada7782a842f76f4a2b9
Chris Clement <clement.chris@icloud.com>
2022-09-09 15:45:07 -0600

    discovered rate limitation on serial port

--- lostik_settings.py
+++ lostik_settings.py
@@ -3,10 +3,9 @@ FIRMWARE_VERSION = 'RN2903 1.0.5 Nov 06 2018 10:45:27'
 
 #Transmit Power (hardware default=2)
 #values: 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 20
-PWR = '16'
+PWR = '2'
 
 #LMODEM robustness options
-
 # if args.power == 'low':
 #     SET_PWR = b'6'
 #     PWR_LABEL = 'LOW'
@@ -23,15 +22,13 @@ PWR = '16'
 #     PWR_LABEL_DBM = '18.5 dBm'
 #     PWR_LABEL_MW = '70.8 mW'
 
-
-
 #Frequency (hardware default=923300000)
 #value range: 902000000 to 928000000
 FREQ = '923300000'
 
 #Spreading Factor (hardware default=sf12)
 #values: sf7, sf8, sf9, sf10, sf11, sf12
-SF = 'sf7'
+SF = 'sf8'
 
 #Radio Bandwidth (KHz) (hardware default=125)
 #values: 125, 250, 500
@@ -39,7 +36,7 @@ BW = '500'
 
 #Coding Rate (FEC) (hardware default=4/5)
 #values: 4/5, 4/6, 4/7, 4/8
-CR = '4/5'
+CR = '4/6'
 
 #Watchdog Timer Time-Out (milliseconds) (hardware default=15000)
 #value range: 0 to 4294967295 (0 disables wdt functionality)
--- notes.md
+++ notes.md
@@ -6,3 +6,24 @@ FIN = File Transfer Finished
 NAK = Not Acknowledged
 ACK = Acknowledged
 TOT = Time-Out Timer
+
+
+Modes:
+
+MODE1 - Fastest & Least Robust
+PWR = 6
+ BW = 500
+ SF = 8
+ CR = 4/6
+
+MODE2 - Middle Ground
+PWR = 12
+ BW = 250
+ SF = 10
+ CR = 4/7
+
+MODE3 - Slowest & Most Robust
+PWR = 17 (or possibly 20)
+ BW = 125
+ SF = 12
+ CR = 4/8
