5a0cb2df0ff8cf86246eca5e91518ce9098f0e70
K7CTC <48072888+K7CTC@users.noreply.github.com>
2022-10-05 08:45:49 -0600

    moved console into ui

--- ui.py
+++ ui.py
@@ -10,11 +10,24 @@
 from sys import exit
 from time import sleep
 
-#local application/library specific imports
-from console import console
+#related third party imports
+from rich.console import Console
+from rich.theme import Theme
 
 if __name__ == '__main__':
     print('[ERROR] ui.py is not intended for direct execution!')
+    exit(1)
+
+console = Console(theme=Theme(inherit=False))
+
+if console.width < 80:
+    print('[ERROR] Terminal width is less than 80 columns!')
+    print('HELP: LMODEM minimal terminal size is 80x24. Resize and try again.')
+    exit(1)
+
+if console.height < 24:
+    print('[ERROR] Terminal height is less than 24 rows!')
+    print('HELP: LMODEM minimal terminal size is 80x24. Resize and try again.')
     exit(1)
 
 def move_cursor(row, column):
